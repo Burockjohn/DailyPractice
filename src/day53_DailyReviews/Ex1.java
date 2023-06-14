fb3afcfb6ed10fe8a128dec0da2f8de953a41ecd
burockjohn <brkcn87@gmail.com>
2023-06-14 23:41:01 +0300

    Day_53 done

--- src/day53_DailyReviews/Ex1.java
+++ src/day53_DailyReviews/Ex1.java
@@ -1,29 +1,32 @@
 package day53_DailyReviews;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Ex1 {
 
     public static void main(String[] args) {
         String test = "hifriekleendsyorilhowareyouzifr";
+        Set<String> result = new HashSet<>();
 
         for (int i = 0; i < test.length(); i++) {
-            String instance = "";
             for (int j = i; j < test.length(); j++) {
                 String temp = test;
                 int counter = 0;
-                instance = test.substring(i, j + 1);
+                String instance = test.substring(i, j + 1);
                 while (temp.contains(instance)) {
                     counter++;
                     temp = temp.replaceFirst(instance, "");
                 }
 
                 if (counter > 1) {
-                    System.out.print(instance + "\t");
+                    result.add(instance);
                 }
 
             }
         }
 
-        //TODO remove duplicates
+        System.out.println(result);
     }
 }
 
